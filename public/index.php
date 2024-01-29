93ba56ac2d267695c1a051d8ac6def31e7d00728
AdrienBeyssonRT <adrienbeysson1@gmail.com>
2024-01-29 19:11:50 +0100

    first commit

--- public/index.php
+++ public/index.php
@@ -1,29 +1,9 @@
 <?php
 
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
+use App\Kernel;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-// Charger les variables d'environnement
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-
-$env = $_SERVER['APP_ENV'] ?? 'dev';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? ('dev' === $env));
-
-// Activer le débogage en mode développement
-if ($debug) {
-    Debug::enable();
-}
-
-// Instancier le kernel
-$kernel = new App\Kernel($env, $debug);
-
-// Gérer la requête
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-
-// Terminer le cycle de vie de la requête
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
